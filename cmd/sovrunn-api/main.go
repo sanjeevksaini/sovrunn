4c9d8ec4ac85750640fe8d85c5c37b92acde8b1c
sanjeevksaini <sanjeevksaini@yahoo.co.in>
2026-07-18 16:19:30 +0530

    feat(FEATURE-0008): complete task 25

--- cmd/sovrunn-api/main.go
+++ cmd/sovrunn-api/main.go
@@ -22,6 +22,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
 	orgRegistry := registry.NewOrganizationRegistry()
 	ouRegistry := registry.NewOrganizationUnitRegistry()
 	ouBlocker := registry.NewOUChildBlockerChecker(ouRegistry)
@@ -41,19 +43,43 @@ func main() {
 	capabilityRegistry := registry.NewCapabilityRegistry()
 	pluginBlocker := registry.NewCapabilityChildBlockerChecker(capabilityRegistry)
 
+	serviceInstanceRegistry := registry.NewServiceInstanceRegistry()
+	serviceBindingRegistry := registry.NewServiceBindingRegistry()
+	capabilityLookup := registry.NewCapabilityLookup(capabilityRegistry)
+	planInstanceBlocker := registry.NewServiceInstancePlanBlockerChecker(serviceInstanceRegistry)
+	projectInstanceBlocker := registry.NewServiceInstanceProjectBlockerChecker(serviceInstanceRegistry)
+
 	orgHandler := api.NewOrgHandler(orgRegistry, ouBlocker, emitter)
 	ouHandler := api.NewOUHandler(ouRegistry, orgRegistry, tenantBlocker, emitter)
 	tenantHandler := api.NewTenantHandler(tenantRegistry, ouRegistry, projectBlocker, emitter)
-	projectHandler := api.NewProjectHandler(projectRegistry, tenantRegistry, emitter)
+	projectHandler := api.NewProjectHandler(projectRegistry, tenantRegistry, emitter, projectInstanceBlocker)
 	operationHandler := api.NewOperationHandler(operationRegistry)
 	serviceClassHandler := api.NewServiceClassHandler(serviceClassRegistry, serviceClassBlocker, emitter)
-	servicePlanHandler := api.NewServicePlanHandler(servicePlanRegistry, serviceClassRegistry, emitter)
+	servicePlanHandler := api.NewServicePlanHandler(servicePlanRegistry, serviceClassRegistry, emitter, planInstanceBlocker)
 	pluginHandler := api.NewPluginHandler(pluginRegistry, serviceClassRegistry, pluginBlocker, emitter)
 	capabilityHandler := api.NewCapabilityHandler(capabilityRegistry, pluginRegistry, serviceClassRegistry, emitter)
+	serviceInstanceHandler := api.NewServiceInstanceHandler(
+		serviceInstanceRegistry,
+		orgRegistry,
+		ouRegistry,
+		tenantRegistry,
+		projectRegistry,
+		serviceClassRegistry,
+		servicePlanRegistry,
+		capabilityLookup,
+		serviceBindingRegistry,
+		emitter,
+		logger,
+	)
+	serviceBindingHandler := api.NewServiceBindingHandler(
+		serviceBindingRegistry,
+		serviceInstanceRegistry,
+		emitter,
+	)
 
 	readiness := &health.ReadinessState{}
 	bootstrapHandler := api.NewBootstrapHandler(cfg, readiness)
-	srv := server.New(cfg, orgHandler, ouHandler, tenantHandler, projectHandler, operationHandler, serviceClassHandler, servicePlanHandler, pluginHandler, capabilityHandler, nil, nil, bootstrapHandler, readiness)
+	srv := server.New(cfg, orgHandler, ouHandler, tenantHandler, projectHandler, operationHandler, serviceClassHandler, servicePlanHandler, pluginHandler, capabilityHandler, serviceInstanceHandler, serviceBindingHandler, bootstrapHandler, readiness)
 
 	if err := srv.Start(); err != nil {
 		log.Printf("server error: %v", err)
